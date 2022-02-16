6c8fd4337c156e2dc23a114c9fca604a83d4b721
Shoma Kobayashi <sho1228.k@gmail.com>
2022-02-16 22:46:50 +0900

    add: BookGraphコンポーネントの追加

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 // Pagesではこのファイルを読み込むことでコンポーネントを使用する
 // TODO: コンポーネントの数が増えた時に、もう一段階細かくわけるかも
-export * from './UserInfoBlock'
-export * from './BookCard'
+export * from './UserInfoBlock';
+export * from './BookCard';
+export * from './BookGraph';
