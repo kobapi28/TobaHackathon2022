082418d536d8b8a9ba3422529625391ae39bb08b
Shoma Kobayashi <sho1228.k@gmail.com>
2022-02-19 17:58:16 +0900

    feat: 一覧ページのUI作成

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,15 +1,55 @@
+import { Box, Image, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import { BookReview } from '../types/BookReview';
 
 // BookGraph内の1つの本を扱うコンポーネント
 type Props = {
   bookReview: BookReview;
 };
+
+type ImageHeightWidth = {
+  height: number;
+  width: number;
+};
+
 export const Book: React.FC<Props> = ({ bookReview }) => {
+  const [imageHeightWidth, setImageHeightWidth] = useState<ImageHeightWidth>({
+    height: 0,
+    width: 0,
+  });
+  const height: number = 250;
+
+  const getImageSize = (e: any) => {
+    setImageHeightWidth({
+      height: e.target.naturalHeight,
+      width: e.target.naturalWidth,
+    });
+  };
+
+  const calculateWidth = (): string => {
+    return `${(imageHeightWidth.width / imageHeightWidth.height) * height}px`;
+  };
+
   return (
-    <div>
-      <h1>{bookReview.book.title}</h1>
-      <h1>{bookReview.book.link}</h1>
-      <img src={bookReview.book.img} alt='bookimage' />
-    </div>
+    <Box position='relative'>
+      <Image
+        id={bookReview.book.link}
+        htmlHeight={height + 'px'}
+        htmlWidth={calculateWidth()}
+        objectFit='cover'
+        src={bookReview.book.img}
+        alt='bookImage'
+        onLoad={(e) => getImageSize(e)}
+      />
+      <Text
+        position='absolute'
+        left='0px'
+        bottom='0px'
+        background='gray.600'
+        width='100%'
+      >
+        {bookReview.review}
+      </Text>
+    </Box>
   );
 };
