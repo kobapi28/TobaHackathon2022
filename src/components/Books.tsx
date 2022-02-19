becf4fbe6e45a3cc0aa679181fb6a63d5e646c3a
Shoma Kobayashi <sho1228.k@gmail.com>
2022-02-19 17:58:16 +0900

    feat: 一覧ページのUI作成

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,16 +1,22 @@
 // BookGraph内の複数の本(Bookコンポーネント)をまとめたコンポーネント
 import { Book } from './Book';
 import { MonthlyBookReviews } from '../types/MonthlyBookReviews';
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { generateYearMonth } from '../utils/generateYearMonth';
 
 type Props = MonthlyBookReviews;
 
 export const Books: React.FC<Props> = ({ yearMonth, bookReviews }) => {
   return (
-    <div>
-      <h1>{yearMonth}</h1>
-      {bookReviews.map((bookReview) => (
-        <Book bookReview={bookReview} key={bookReview.readAt.toString()} />
-      ))}
-    </div>
+    <Box>
+      <Flex gap='8px'>
+        {bookReviews.map((bookReview) => (
+          <Book bookReview={bookReview} key={bookReview.readAt.toString()} />
+        ))}
+      </Flex>
+      <Text margin='auto'>
+        {yearMonth === generateYearMonth(new Date()) ? 'now' : yearMonth}
+      </Text>
+    </Box>
   );
 };
