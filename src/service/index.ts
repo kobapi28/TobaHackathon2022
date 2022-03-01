4f764d3b5c374f9a5a12b3e593c13917400c234d
oonya <oonya0816@icloud.com>
2022-03-01 22:19:30 +0900

    feat: 雑にBookReviewClient.searchを実装した

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,25 +1,39 @@
+import axios from 'axios';
 import { IBookReviewClient, IAuthClient } from './Interface';
 import { Book } from '../types/Book';
 import { login, logout } from './Auth';
 import { getBookReviews, storeBookReview } from './BookReviewClient';
+
+type Res = {
+  Items: Array<Item>;
+};
+type Item = {
+  Item: {
+    affiliateUrl: string;
+    author: string;
+    itemUrl: string;
+    smallImageUrl: string; //largeImageUrl, mediumImageUrl
+    title: string;
+  };
+};
+
 const searchMock = async (title: string): Promise<Array<Book>> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve([
-        {
-          title: 'Team Geek ―Googleのギークたちはいかにしてチームを作るのか',
-          img: 'https://images-na.ssl-images-amazon.com/images/I/41SlY0zvpKL._SX350_BO1,204,203,200_.jpg',
-          link: 'https://www.amazon.co.jp/dp/4873116309/?coliid=I1BCLDLBL7US62&colid=10WYP57Z19TPE&psc=1&ref_=lv_ov_lig_dp_it',
-        },
-        {
-          title:
-            'SCRUM BOOT CAMP THE BOOK【増補改訂版】 スクラムチームではじめるアジャイル開発',
-          img: 'https://images-na.ssl-images-amazon.com/images/I/71591oKcHWL.jpg',
-          link: 'https://www.amazon.co.jp/-/en/dp/4798163686/?coliid=I3C3Y6TXT3F0BX&colid=10WYP57Z19TPE&psc=1&ref_=lv_ov_lig_dp_it',
-        },
-      ]);
-    }, 1000);
+  const applicationId = process.env.REACT_APP_RAKUTEN_APP_ID;
+  const url = `https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?applicationId=${applicationId}&title=${encodeURI(
+    title
+  )}`;
+
+  let books: Array<Book>;
+  const { data } = await axios.get<Res>(url);
+  books = data.Items.map((i) => {
+    return {
+      title: i.Item.title,
+      link: i.Item.itemUrl,
+      img: i.Item.smallImageUrl,
+    };
   });
+
+  return books;
 };
 
 export const BookReviewClient: IBookReviewClient = {
