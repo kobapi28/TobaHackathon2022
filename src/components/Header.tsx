63f4958d3d6fe50196a6693e263935565bdb60e4
Shoma Kobayashi <sho1228.k@gmail.com>
2022-03-03 00:35:26 +0900

    feat: HeaderにLogoutボタンを置いてログアウトできるように実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,23 @@ import {
   Avatar,
   useDisclosure,
 } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../providers';
 import { AddNewModal } from './modal/AddNewModal';
 
 export const Header = () => {
   const auth = useAuth();
+  const navigate = useNavigate();
   const clickUserAvatar = () => {
     // ユーザーページへ遷移
+    if (auth.user?.name) {
+      navigate(`users/${auth.user?.id}`);
+    }
     console.log('ユーザーページへの遷移');
+  };
+
+  const logout = () => {
+    auth.logout(() => navigate('/about'));
   };
 
   // modal操作
@@ -45,6 +54,9 @@ export const Header = () => {
             ></Avatar>
             <Button size='sm' mx='4' onClick={() => onOpen()}>
               add new
+            </Button>
+            <Button size='sm' mx='4' onClick={() => logout()}>
+              logout
             </Button>
           </Box>
         ) : (
